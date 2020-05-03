dd01b41c71d24ad46871f43fbe4d6ddd7b1c16ae
Anirban Bera <anirban.bera.89@gmail.com>
2020-05-04 00:58:14 +0530

    #Added category card. (incomplete) Added search and filter bar (incomplete)

--- src/components/ForumLanding/ForumLanding.js
+++ src/components/ForumLanding/ForumLanding.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import QuestionPreviewFeed from '../QuestionPreviewFeed/QuestionPreviewFeed';
+import CategoryCard from '../CategoryCards/CategoryCard';
+import SearchFilterBar from '../SearchFilterBar/SearchFilterBar';
 
 const ForumLanding = () => {
   return (
     <Grid container columns="equal" stackable>
       <Grid.Row>
+        <SearchFilterBar />
+      </Grid.Row>
+      <Grid.Row>
         <Grid.Column width={12}>
           <QuestionPreviewFeed />
         </Grid.Column>
-        <Grid.Column width={4}>colmn right</Grid.Column>
+        <Grid.Column width={4}>
+          <CategoryCard />
+        </Grid.Column>
       </Grid.Row>
     </Grid>
   );
