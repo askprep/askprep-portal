3c4868c9c5ae2e2a50a4b92165c7e1ab83aa9ea1
Animesh Lohar <animesh.lohar@pwc.com>
2020-05-25 03:26:37 +0530

    rects draft

--- src/components/discussion/discussion.jsx
+++ src/components/discussion/discussion.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RichTextEditor from './RichTextEditor';
-
+import Rects from '../rects/rects';
 export const Discussion = () => {
-  return <RichTextEditor />;
+  return <Rects />;
 };
---
+++ src/components/rects/rects.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { Grid, Button, Icon } from 'semantic-ui-react';
+import { rectangles } from '../discussion/sample';
+import './rects.css';
+
+const Rects = () => {
+  const [rectangleTypes, setrectangleTypes] = useState([...rectangles]);
+  const changeType = (type, index) => {
+    switch (type) {
+      case 'image':
+        setrectangleTypes([
+          ...rectangleTypes,
+          (rectangleTypes[index].type = 'image'),
+        ]);
+        break;
+      case 'text':
+        setrectangleTypes([
+          ...rectangleTypes,
+          (rectangleTypes[index].type = 'text'),
+        ]);
+        break;
+      case 'remove':
+        setrectangleTypes([
+          ...rectangleTypes,
+          (rectangleTypes[index].type = ''),
+        ]);
+        break;
+
+      default:
+        break;
+    }
+  };
+  return (
+    <Grid.Column width={4}>
+      <div
+        style={{
+          position: 'relative',
+          display: 'inline - block',
+        }}
+      >
+        <img src={require('./IES_Clean.jpg')} />
+        {rectangleTypes &&
+          rectangleTypes.length > 0 &&
+          rectangleTypes.map((item, index) => {
+            return (
+              <>
+                <div
+                  className="selectedSection"
+                  key={index}
+                  style={{
+                    position: 'absolute',
+                    zIndex: 999,
+                    margin: '0 auto',
+                    left: `${item.left}px`,
+                    right: `${item.right}px`,
+                    textAlign: 'center',
+                    top: `${item.top}px`,
+                    border: '2px solid lightgreen',
+                    fontFamily: 'Arial,sans-serif',
+                    width: `${item.width}px`,
+                    height: `${item.height}px`,
+                  }}
+                >
+                  <span className="edit">
+                    <Button.Group>
+                      <Button
+                        onClick={() => changeType('image', index)}
+                        positive={item.type === 'image' ? true : false}
+                      >
+                        Image
+                      </Button>
+                      <Button.Or />
+                      <Button
+                        onClick={() => changeType('text', index)}
+                        positive={item.type === 'text' ? true : false}
+                      >
+                        Text
+                      </Button>
+                      <Button.Or />
+                      <Button
+                        onClick={() => changeType('remove', index)}
+                        color="red"
+                      >
+                        Remove
+                      </Button>
+                    </Button.Group>
+                    {/* <a href="#">
+                      <Icon name="image" size="large" />
+                    </a>
+                    <a href="#">
+                      <Icon name="edit outline" size="large" />
+                    </a> */}
+                  </span>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </Grid.Column>
+  );
+};
+
+export default Rects;
